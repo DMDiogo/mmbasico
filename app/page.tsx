0c62440f1d2a4d8735efb687b9c4aaa9b99a1ef0
dhdhsadj <maguinhomast2005@gmail.com>
2025-07-28 19:55:29 +0100

    experimento

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
- const HomePage = ()=> {
+const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
- }
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Bem-vindo à HomePage</h1>
+    </div>
+  );
+};
 
- export default HomePage;
+export default HomePage;
