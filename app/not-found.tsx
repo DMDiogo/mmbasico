0398199f4e39a8a3904ecad97d6711d2f10f0da9
dhdhsadj <maguinhomast2005@gmail.com>
2025-08-03 19:54:48 +0100

    experimento

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound (){
     return(
         <div className='flex flex-col items-center justify-center min-h-screen'>
             <h1 className='text-center text-4xl font-bold mt-9 '>
-                Página Não Encontrada! Error: 404
+                Página Não Encontrada! Error: 404.
             </h1>
             <p className='mt-4'>A página que tento acessar não existe</p>
             <Link href="/"className='mt-4 text-blue-500 underline'>
