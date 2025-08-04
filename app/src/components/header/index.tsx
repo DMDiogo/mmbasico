a18a9d9f721ba07c6c94bd690ad9a81e809d97f8
dhdhsadj <maguinhomast2005@gmail.com>
2025-08-04 20:57:10 +0100

    experimento

--- app/src/components/header/index.tsx
+++ app/src/components/header/index.tsx
@@ -9,7 +9,7 @@ export function header() {
         <nav>
           <ul className='flex items-center justify-center gap-4'>
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/">Home.</Link>
             </li>
             <li>
               <Link href="/login">Login</Link>
